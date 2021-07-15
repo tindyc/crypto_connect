2a21f1db5af1c5fbb16ccc77251a8689f7a69131
tindyc <tindyc@bath.edu>
2021-07-15 15:01:37 +0000

    hide flass messages after 2 secs

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// Hide flash messages after 2secs
+setTimeout(()=> {
+    flash_message = document.getElementsByClassName("flash");
+    for (let i = 0; i < flash_message.length; i++) {
+        flash_message[i].style.display="none";
+    }
+}, 2000);
