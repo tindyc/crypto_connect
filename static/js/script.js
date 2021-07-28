dfb0997ca074d82f9fc774f324c92042d04b6780
tindyc <tindyc@bath.edu>
2021-07-28 22:43:55 +0000

    remove unused code from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Hide flash messages after 2secs
-setTimeout(()=> {
+setTimeout(() => {
     flash_message = document.getElementsByClassName("flash");
     for (let i = 0; i < flash_message.length; i++) {
-        flash_message[i].style.display="none";
+        flash_message[i].style.display = "none";
     }
 }, 2000);
